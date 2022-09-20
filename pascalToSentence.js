323ded149cfee423605e971ffdee8cb585a43a50
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-09-20 17:00:32 +0000

    reverse added

--- pascalToSentence.js
+++ pascalToSentence.js
@@ -15,3 +15,20 @@ function pascalToSentence(text){
   
   pascalToSentence('WelcomeToAccra')
   
+
+  /**
+   * reversepascalToString - Reverses a  pascal case string
+   * @param {String} text - Stirng to be reversed
+   * @returns String
+   */
+  function reversepascalToString(text){
+    let arr = text.split('');
+    let result = '';
+    for (let i = arr.length - 1; i >= 0 ; i--)
+     {
+       result += (arr[i].toUpperCase() === arr[i] && i > 0) ? arr[i].toLowerCase() + ' ' : arr[i];
+     }
+     return result;
+   }
+   
+   console.log(reversepascalToString("WelcomeToBuwelo"));
